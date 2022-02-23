d4de5c0b46c186a9abd84796556e5e7b02039b38
Luciano Macuglia <lukymacuglia@gmail.com>
2022-02-22 21:24:29 -0300

    Cargamos los archivos del projecto ya empezado de Django

---
+++ ProYT/ProYT/urls.py
@@ -0,0 +1,30 @@
+"""ProYT URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from ProYT.views import Principal, Comenter, FexaDoy, Calculef
+
+urlpatterns = [
+    path('admin/', admin.site.urls), #Url propiciada por Django
+    path('Home/', Principal), #Url con el menu para elegir que hacer
+    path('CajCome/', Comenter),
+    path('Fexa/', FexaDoy), # Contenido cambiante en cada actualizacion
+    path("An/<int:agno>", Calculef) #Vamos a pasar por la URL un parametro
+]
+
+
+# El proposito de este proyecto es que Melo (Mi novia) escriba en una caja de texto cualquier queja que tenga en
+# relacion a la experiencia de noviazgo conmigo, el comentario se envia a mi mail y de esa manera podre tomar medidas
---
+++ ProYT/ProYT/views.py
@@ -0,0 +1,46 @@
+
+from django.http import HttpResponse
+from django.template import Template, Context
+import datetime
+
+#Aca van planchadas las distintas paginas y adornos y etc
+
+class Person(object):
+	def __init__(self, Nombre, Apellido):
+		self.nombre=Nombre
+		self.apellido=Apellido
+
+def  Principal(request):
+	MeinMenu = ["Quejas", "Mensajes", "Puntuacion"]
+	#Nombre = "Melody"
+	#Apellido = "Trilla"
+	p1 = Person("Melody", "Trilla")
+	Est = datetime.datetime.now()
+
+	DoxOut = open(r"D:/Users/lukym/Desktop/Projects/Dproject/Front/Home.html")
+	plt = Template(DoxOut.read())
+	DoxOut.close()
+
+	ctx = Context({"Nombre":p1.nombre, "Apellido":p1.apellido, "Fexa":Est, "Menu":MeinMenu})
+	Docc = plt.render(ctx)
+
+	return HttpResponse(Docc)
+
+def Comenter(request):
+	return HttpResponse("Caja de comentario con boton de enviar")
+
+#Ejemplo de contenido dinamico
+def FexaDoy(request):
+	Fexa = datetime.datetime.now()
+	return HttpResponse("Fecha y Hora de hoy dia %s" %Fexa)
+
+#Pasar parametro por URL
+
+def Calculef(request, agno = 2019):
+	edadACT = 18
+	Periodo = agno - 2022
+	Edfur = edadACT + Periodo
+
+	return HttpResponse("En el el año %s tendras %s años" %(agno, Edfur))
+
+#El uso de las plantillas
